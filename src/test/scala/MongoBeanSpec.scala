c56f2e98e00c3cc13a28869054562ed8047b930f
Brian Lawler <bplawler@gmail.com>
2012-11-12 22:38:29 -0800

    added the tests for the special behavior of _id

--- src/test/scala/MongoBeanSpec.scala
+++ src/test/scala/MongoBeanSpec.scala
@@ -9,6 +9,15 @@ class HelloWorldSpec extends Specification { def is =           sequential ^
     "allow me to set and get String properties"                            ! e1^
     "but should not save anything to the database"                         ! e2^
     "until the save method is invoked on the bean implementation"          ! e3^
+                                                                           p^
+  "The _id attribute has special behavior.  It should"                     ^
+    "not be set for new instances of mongobeans"                           ! e4^
+    "be automatically updated when a new bean is saved to the collection"  ! e5^
+    "if set to a value by the application, a call to load should load"     +
+      "the corresponding document behind this bean"                        ! e6^
+                                                                           p^
+  "Concurrent modifications to the same document should"                   ^
+    "work correctly"                                                       ! e7^
                                                                            end
     
   def e1 = {
@@ -18,10 +27,37 @@ class HelloWorldSpec extends Specification { def is =           sequential ^
 
   def e2 = {
     val d = defaultDeal
-    Config.deals.count() must_== 0
+    Config.deals.count(MongoDBObject()) must_== 0
   }
 
-  def e3 = pending
+  def e3 = {
+    val d = defaultDeal
+    d.save
+    Config.deals.count(MongoDBObject()) must_== 1
+  }
+
+  def e4 = {
+    val d = defaultDeal
+    !(d._id.value isDefined)
+  }
+  
+  def e5 = {
+    val d = defaultDeal
+    d.save
+    d._id.value.isDefined
+  }
+
+  def e6 = {
+    val d = defaultDeal
+    d.save
+    val _id = d._id.value
+    val newDeal = new Deal
+    newDeal._id.value = _id.get
+    newDeal.load
+    newDeal.source.value must_== d.source.value
+  }
+
+  def e7 = pending
 
   implicit val before: Context = new Before { 
     def before = Config.deals.remove(MongoDBObject())
